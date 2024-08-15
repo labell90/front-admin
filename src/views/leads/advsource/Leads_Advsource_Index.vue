3c3ac190adce5c7544b6283f350a06967d44059a
Mehdi Kord <mehdi.kord171@gmail.com>
2024-08-15 14:03:26 +0330

    [leads] update validation

--- src/views/leads/advsource/Leads_Advsource_Index.vue
+++ src/views/leads/advsource/Leads_Advsource_Index.vue
@@ -199,7 +199,7 @@ export default {
         <template v-slot:body-cell-tools="props">
           <q-td :props="props">
             <div class="text-center">
-              <q-btn :to="{name:'lead_industries_edit',params:{id:props.row.id}}" glossy title="ویرایش آیتم" class="q-ma-xs" color="blue-8" icon="fas fa-edit" size="11px" round  />
+              <q-btn :to="{name:'lead_advsource_edit',params:{id:props.row.id}}" glossy title="ویرایش آیتم" class="q-ma-xs" color="blue-8" icon="fas fa-edit" size="11px" round  />
               <global_actions_delete_item @Set_Ok="Item_Delete(props.row.id)" :loading="delete_loading"></global_actions_delete_item>
             </div>
           </q-td>
