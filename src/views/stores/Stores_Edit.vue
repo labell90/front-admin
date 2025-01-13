e2d899ed33d7f1a84a5600de81ddd955a9843f8c
Mehdi Kord <mehdi.kord171@gmail.com>
2025-01-13 12:20:42 +0330

    fixed bug in stores edit

--- src/views/stores/Stores_Edit.vue
+++ src/views/stores/Stores_Edit.vue
@@ -262,7 +262,7 @@ export default {
           province_id : this.items.province_id
         }
         this.Module_Location_Action_City_Selectable(items).then(response => {
-          this.items.city_id = null;
+          //this.items.city_id = null;
           this.cities = response;
         });
       }
